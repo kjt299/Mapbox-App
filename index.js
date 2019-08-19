30d326c6b1af5e42409f62ec7bb91f17d7447eed
kjt299 <47479824+kjt299@users.noreply.github.com>
2019-08-19 17:42:19 +0100

    populate sidebar

--- index.js
+++ index.js
@@ -23,6 +23,7 @@ function setUpMap() {
 //display map on load
 window.onload = function() {
     setUpMap();
+    populateSidebar(geojson);
 }
 
 //store markers
@@ -35,7 +36,8 @@ const geojson = {
         coordinates: [-0.140634, 51.501476]
       },
       properties: {
-        title: 'Buckingham Palace'
+        title: 'Buckingham Palace',
+        address:'sample'
       }
     },
     {
@@ -45,7 +47,8 @@ const geojson = {
         coordinates: [-0.119519, 51.503399]
       },
       properties: {
-        title: 'London Eye'
+        title: 'London Eye',
+        address:'sample'
       }
     },
     {
@@ -55,7 +58,8 @@ const geojson = {
           coordinates: [-0.116773, 51.510357]
         },
         properties: {
-          title: 'Big Ben'
+          title: 'Big Ben',
+          address:'sample'
         }
       },
       {
@@ -65,7 +69,8 @@ const geojson = {
           coordinates: [-0.076132, 51.508530]
         },
         properties: {
-          title: 'The Tower of London'
+          title: 'The Tower of London',
+          address:'sample'
         }
       },
       {
@@ -75,7 +80,8 @@ const geojson = {
           coordinates: [-0.126168, 51.518757]
         },
         properties: {
-          title: 'British Museum'
+          title: 'British Museum',
+          address:'sample'
         }
       },
       {
@@ -85,7 +91,8 @@ const geojson = {
           coordinates: [-0.098056, 51.513611]
         },
         properties: {
-          title: 'St Paul\'s Cathedral'
+          title: 'St Paul\'s Cathedral',
+          address:'sample'
         }
       }]
 };
@@ -104,3 +111,25 @@ function addMarkers(map) {
       .addTo(map);
   });
 } 
+
+//populate sidebar
+function populateSidebar(data) {
+  // Iterate through the list of landmarks
+  for (i = 0; i < data.features.length; i++) {
+    let currentFeature = data.features[i];
+    // Select the listing container in the HTML and append a div with the class 'item' for each store
+    let landmarks = document.getElementById('landmarks');
+    let landmark = landmarks.appendChild(document.createElement('div'));
+    landmark.className = 'item';
+    landmark.id = 'landmark-' + i;
+
+    const title = landmark.appendChild(document.createElement('div'));
+    title.className = 'title';
+    title.innerHTML = currentFeature.properties.title;
+
+    // Create a new div with the class 'details' for each store and fill it with the title and description
+    const address = landmark.appendChild(document.createElement('div'));
+    address.className = 'address';
+    address.innerHTML = currentFeature.properties.address;
+  }
+}
