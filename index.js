f319aec2c4bc9ee169cd73ce65c84cf9ef9c3b3f
kjt299 <47479824+kjt299@users.noreply.github.com>
2019-08-25 21:54:17 +0100

    clean and refactor code

--- index.js
+++ index.js
@@ -1,10 +1,93 @@
-//restricting map view to London only
+//Store markers
+const geojson = {
+  type: 'FeatureCollection',
+  features: [{
+    type: 'Feature',
+    geometry: {
+      type: 'Point',
+      coordinates: [-0.140634, 51.501476]
+    },
+    properties: {
+      title: 'Buckingham Palace',
+      address:'Westminster, London SW1A 1AA',
+      id:'1',
+      markerId:'marker-1'
+    }
+  },
+  {
+    type: 'Feature',
+    geometry: {
+      type: 'Point',
+      coordinates: [-0.119519, 51.503399]
+    },
+    properties: {
+      title: 'London Eye',
+      address:'Lambeth, London SE1 7PB',
+      id: '2',
+      markerId:'marker-2'
+    }
+  },
+  {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [-0.116773, 51.510357]
+      },
+      properties: {
+        title: 'Big Ben',
+        address:'Westminster, London SW1A 0AA',
+        id:'3',
+        markerId:'marker-3'
+      }
+    },
+    {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [-0.076132, 51.508530]
+      },
+      properties: {
+        title: 'The Tower of London',
+        address:'St Katharine\'s & Wapping, London EC3N 4AB',
+        id:'4',
+        markerId:'marker-4'
+      }
+    },
+    {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [-0.126168, 51.518757]
+      },
+      properties: {
+        title: 'British Museum',
+        address:'Great Russell St, Bloomsbury, London WC1B 3DG',
+        id:'5',
+        markerId:'marker-5'
+      }
+    },
+    {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [-0.098056, 51.513611]
+      },
+      properties: {
+        title: 'St Paul\'s Cathedral',
+        address:'St. Paul\'s Churchyard, London EC4M 8AD',
+        id:'6',
+        markerId:'marker-6'
+      }
+    }]
+};
+
+//Restricting map view to London only
 const bounds = [[-0.8879165136707741, 51.19892541726361],
                 //southwest coordinates];
                 [0.6364365136759886,  51.81604491419381]];
                 //northeast coordinates
 
-//map setup
+//Map setup
 function setUpMap() {
     mapboxgl.accessToken = 'pk.eyJ1Ijoia2p0Mjk5IiwiYSI6ImNqemZpbWxjMjBjazczbnFrOG40dWlyaXgifQ.s9W5PFPvWY0cSmZsqioqdg';
     const map = new mapboxgl.Map({
@@ -20,105 +103,20 @@ function setUpMap() {
         addMarkers(map);
 }
 
-//display map on load
+//Display map on load
 window.onload = function() {
     setUpMap();
-    populateSidebar(geojson);
+    populateSidebar();
 }
 
-//store markers
-const geojson = {
-    type: 'FeatureCollection',
-    features: [{
-      type: 'Feature',
-      geometry: {
-        type: 'Point',
-        coordinates: [-0.140634, 51.501476]
-      },
-      properties: {
-        title: 'Buckingham Palace',
-        address:'Westminster, London SW1A 1AA',
-        id:'1',
-        markerID:'1id'
-      }
-    },
-    {
-      type: 'Feature',
-      geometry: {
-        type: 'Point',
-        coordinates: [-0.119519, 51.503399]
-      },
-      properties: {
-        title: 'London Eye',
-        address:'Lambeth, London SE1 7PB',
-        id: '2',
-        markerID:'2id'
-      }
-    },
-    {
-        type: 'Feature',
-        geometry: {
-          type: 'Point',
-          coordinates: [-0.116773, 51.510357]
-        },
-        properties: {
-          title: 'Big Ben',
-          address:'Westminster, London SW1A 0AA',
-          id:'3',
-          markerID:'3id'
-        }
-      },
-      {
-        type: 'Feature',
-        geometry: {
-          type: 'Point',
-          coordinates: [-0.076132, 51.508530]
-        },
-        properties: {
-          title: 'The Tower of London',
-          address:'St Katharine\'s & Wapping, London EC3N 4AB',
-          id:'4',
-          markerID:'4id'
-        }
-      },
-      {
-        type: 'Feature',
-        geometry: {
-          type: 'Point',
-          coordinates: [-0.126168, 51.518757]
-        },
-        properties: {
-          title: 'British Museum',
-          address:'Great Russell St, Bloomsbury, London WC1B 3DG',
-          id:'5',
-          markerID:'5id'
-        }
-      },
-      {
-        type: 'Feature',
-        geometry: {
-          type: 'Point',
-          coordinates: [-0.098056, 51.513611]
-        },
-        properties: {
-          title: 'St Paul\'s Cathedral',
-          address:'St. Paul\'s Churchyard, London EC4M 8AD',
-          id:'6',
-          markerID:'6id'
-        }
-      }]
-};
-
-let markers = [];
-// add markers to map
+// Add markers to the map
 function addMarkers(map) {
     geojson.features.forEach(function(marker) {
-      markers.push(marker);
-    // create a HTML element for each feature
-    var el = document.createElement('div');
+    // Create a HTML element for each feature
+    let el = document.createElement('div');
     el.className = 'marker';
-    el.id = marker.properties.id + "id";
-    // make a marker for each feature and add to the map
+    el.id = "marker-" + marker.properties.id;
+    // Make a marker for each feature and add to the map
     new mapboxgl.Marker(el)
       .setLngLat(marker.geometry.coordinates)
       .setPopup(new mapboxgl.Popup({ offset: 25 }) // add popups
@@ -128,54 +126,42 @@ function addMarkers(map) {
 } 
 
 //populate sidebar
-function populateSidebar(data) {
+function populateSidebar() {
   // Iterate through the list of landmarks
-  for (i = 0; i < data.features.length; i++) {
-    let currentFeature = data.features[i];
+  for (i = 0; i < geojson.features.length; i++) {
     // Select the listing container in the HTML and append a div with the class 'item' for each landmark
     let landmarks = document.getElementById('landmarks');
     let landmark = landmarks.appendChild(document.createElement('div'));
     landmark.className = 'item';
-    landmark.id = currentFeature.properties.id;
+    landmark.id = geojson.features[i].properties.id;
 
     // Create a new link with the class 'title' for each landmark
-    const title = landmark.appendChild(document.createElement('p'));
+    let title = landmark.appendChild(document.createElement('p'));
     title.className = 'title';
-    title.innerHTML = currentFeature.properties.title;
+    title.innerHTML = geojson.features[i].properties.title;
 
     // Create a new div with the class 'details' for each store and fill it with the title and description
-    const address = landmark.appendChild(document.createElement('p'));
+    let address = landmark.appendChild(document.createElement('p'));
     address.className = 'address';
-    address.innerHTML = currentFeature.properties.address;
+    address.innerHTML = geojson.features[i].properties.address;
   }
 }
 
 function filterResults() {
-  const landmarks = [];
-  for( let i = 0; i < geojson.features.length; i++){
+  let landmarks = [];
+  for (let i = 0; i < geojson.features.length; i++){
     landmarks.push(geojson.features[i].properties.title); 
   }
   let input = document.getElementById('myInput').value;
-  console.log("input: " + input);
-  console.log(landmarks);
-
   // Loop through all list items, and hide those who don't match the search query
   for (let i = 0; i < landmarks.length; i++) {
     if (!landmarks[i].toLowerCase().includes(input.toLowerCase())) {
       document.getElementById(geojson.features[i].properties.id).style.display = "none";
-      document.getElementById(geojson.features[i].properties.markerID).style.backgroundImage = "none";
+      document.getElementById(geojson.features[i].properties.markerId).style.backgroundImage = "none";
     }
     else{
       document.getElementById(geojson.features[i].properties.id).style.display = "";
-      document.getElementById(geojson.features[i].properties.markerID).style.backgroundImage = "";
+      document.getElementById(geojson.features[i].properties.markerId).style.backgroundImage = "";
     }
   }
 }
-
-/*
-console.log(document.getElementById(i + "id"));
-document.getElementById(i + "id").style.backgroundImage = "none";
- console.log(document.getElementById(i + "id"));
-      document.getElementById(i + "id").style.backgroundImage = "none";
-
-*/
